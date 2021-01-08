033a24c42ba5a58963325e176cb5165b5147ae93
Maciej <maciej.tokarz1@wp.pl>
2021-01-08 18:14:31 +0100

    test bad surname metod addClient in class Client REDR

--- tests/test_add_client.py
+++ tests/test_add_client.py
@@ -11,6 +11,10 @@ class testAddClient(TestCase):
         result = self.temp.addClient
         self.assertRaisesRegex(TypeError, "Bad type name", result, 12345, str, str)
 
+    def test_add_client_bad_surname(self):
+        result = self.temp.addClient
+        self.assertRaisesRegex(TypeError, "Bad type surname", result, "Ola", False, str)
+
     def tearDown(self):
         self.temp = None
 
