2bd3ac5bcd778ec154e68438da5f00aeffec5fcd
Maciej <maciej.tokarz1@wp.pl>
2021-01-08 21:05:35 +0100

    test bad surname, metod editClient in class Client RED

--- tests/test_edit_client.py
+++ tests/test_edit_client.py
@@ -18,6 +18,10 @@ class testEditClient(TestCase):
         result = self.temp.editClient
         self.assertRaisesRegex(TypeError, "Bad type new name", result, 4, False, str, str)
 
+    def test_edit_client_bad_new_surname(self):
+        result = self.temp.editClient
+        self.assertRaisesRegex(TypeError, "Bad type new surname", result, 1, "Adam", 1234, str)
+
     def tearDown(self):
         self.temp = None
 
