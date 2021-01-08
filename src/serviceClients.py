9e496a5b28c00736afbb1fc66ad27caf04665163
Maciej <maciej.tokarz1@wp.pl>
2021-01-08 20:45:34 +0100

    test delete positive, metod deleteClient in class Client REFACTOR

--- src/serviceClients.py
+++ src/serviceClients.py
@@ -25,9 +25,10 @@ class Client:
             return self.ClientStorage.postClient(name, surname, email)
 
     def deleteClient(self, id_client):
+        clients = self.ClientStorage.getAllClients()
         if type(id_client) != int:
             raise TypeError("Bad type id client")
-        if len(list(filter(lambda client: client["id"] == id_client, self.ClientStorage.getAllClients()))) != 1:
+        if len(list(filter(lambda client: client["id"] == id_client, clients))) != 1:
             raise Exception("This client not exist in data base")
         else:
             return self.ClientStorage.delClient(id_client)
