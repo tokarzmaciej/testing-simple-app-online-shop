b850c908ed92c02ab107c6d75c7b27e0b48967c0
Maciej <maciej.tokarz1@wp.pl>
2021-01-09 12:03:17 +0100

    test product not exists, method editProduct in class Product RED

--- tests/test_edit_product.py
+++ tests/test_edit_product.py
@@ -1,11 +1,13 @@
 from unittest.mock import *
 from unittest import TestCase, main
 from src.serviceProducts import Product
+from src.dataProducts import ProductsData
 
 
 class testEditProduct(TestCase):
     def setUp(self):
         self.temp = Product()
+        self.products = ProductsData().products
 
     # dummy
     def test_edit_product_bad_id(self):
@@ -21,3 +23,17 @@ class testEditProduct(TestCase):
         self.temp.editProduct.side_effect = TypeError("Bad type new value")
         result = self.temp.editProduct
         self.assertRaisesRegex(TypeError, "Bad type new value", result, 7, "kite", "22")
+
+    def test_edit_product_not_exist(self):
+        self.temp.ProductStorage.getAllProducts = Mock()
+        self.temp.ProductStorage.getAllProducts.return_value = self.products
+
+        result = self.temp.editProduct
+        self.assertRaisesRegex(Exception, "This product not exist in data base", result, 20, "bike", 1500)
+
+    def tearDown(self):
+        self.temp = None
+
+
+if __name__ == '__main__':
+    main()
