4608ecbb11d56b1979688b8eefe2fd53fb8c07ed
Maciej <maciej.tokarz1@wp.pl>
2021-01-09 21:49:48 +0100

    test lack order, method getProductsInOrder in class Order

--- tests/test_get_order.py
+++ tests/test_get_order.py
@@ -1,15 +1,36 @@
 from unittest.mock import *
 from unittest import TestCase, main
 from src.serviceOrders import Order
+from src.dataProducts import ProductsData
+from src.dataProductsOrders import OrdersProductsData
+from src.dataOrders import OrdersData
 
 
 class testGetProductsInOrder(TestCase):
     def setUp(self):
         self.temp = Order()
+        self.products = ProductsData().products
+        self.productsOrders = OrdersProductsData().productsOrders
+        self.orders = OrdersData().orders
 
     def test_get_products_in_order_bad_id(self):
         result = self.temp.getProductsInOrder
         self.assertRaisesRegex(TypeError, "Bad type order id", result, "one")
+
+    @patch('src.serviceOrders.ProductStorage')
+    def test_get_products_in_order_lack_order(self, products):
+        instance_products = Mock()
+        instance_products.getAllProducts.return_value = self.products
+        products.return_value = instance_products
+
+        self.temp.OrderStorage.getAllOrdersProducts = Mock()
+        self.temp.OrderStorage.getAllOrdersProducts.return_value = self.productsOrders
+
+        self.temp.OrderStorage.getAllOrders = Mock()
+        self.temp.OrderStorage.getAllOrders.return_value = self.orders
+
+        result = self.temp.getProductsInOrder
+        self.assertRaisesRegex(Exception, "This order not exist in data base", result, 150)
 
     def tearDown(self):
         self.temp = None
