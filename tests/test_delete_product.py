41fbd0c365501ebb760fc525a663c60bdc4a385a
Maciej <maciej.tokarz1@wp.pl>
2021-01-09 11:16:27 +0100

    test lack product, method deleteProduct in class Product RED

--- tests/test_delete_product.py
+++ tests/test_delete_product.py
@@ -1,15 +1,24 @@
 from unittest.mock import *
 from unittest import TestCase, main
 from src.serviceProducts import Product
+from src.dataProducts import ProductsData
 
 
 class testDeleteProduct(TestCase):
     def setUp(self):
         self.temp = Product()
+        self.products = ProductsData().products
 
     def test_delete_product_bad_id(self):
         result = self.temp.deleteProduct
         self.assertRaisesRegex(TypeError, "Bad type id product", result, False)
+
+    def test_delete_product_lack_product(self):
+        self.temp.ProductStorage.getAllProducts = Mock()
+        self.temp.ProductStorage.getAllProducts.return_value = self.products
+
+        result = self.temp.deleteProduct
+        self.assertRaisesRegex(Exception, "This product not exist in data base", result, 88)
 
     def tearDown(self):
         self.temp = None
