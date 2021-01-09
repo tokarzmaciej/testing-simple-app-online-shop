2eb3724bea697987da18f40cf08c85fb3fc5ba57
Maciej <maciej.tokarz1@wp.pl>
2021-01-09 15:05:22 +0100

    test lack client in base, method addOrder in class Order REFACTOR

--- src/serviceOrders.py
+++ src/serviceOrders.py
@@ -1,5 +1,6 @@
 from src.baseClients import ClientStorage
 
+
 class Order:
     def __init__(self):
         self.ClientStorage = ClientStorage()
@@ -7,5 +8,5 @@ class Order:
     def addOrder(self, id_client, cart):
         if type(id_client) != int:
             raise TypeError("Bad type id")
-        else:
+        if len(list(filter(lambda client: client["id"] == id_client, self.ClientStorage.getAllClients()))) != 1:
             raise Exception("This client not exist in data base")
