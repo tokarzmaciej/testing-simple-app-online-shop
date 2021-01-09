c367d018301d7be2c270410bae938bb31c0e9a25
Maciej <maciej.tokarz1@wp.pl>
2021-01-09 10:56:25 +0100

    test add product positive, method addProduct in class Product REFACTOR

--- src/serviceProducts.py
+++ src/serviceProducts.py
@@ -6,12 +6,12 @@ class Product:
         self.ProductStorage = ProductStorage()
 
     def addProduct(self, name, value):
+        products = self.ProductStorage.getAllProducts()
         if type(name) != str:
             raise TypeError("Bad type name")
         if type(value) != int and type(value) != float:
             raise TypeError("Bad type value")
-        if list(filter(lambda product: product["name"] == name, self.ProductStorage.getAllProducts())):
+        if list(filter(lambda product: product["name"] == name, products)):
             raise Exception("This product exist")
         else:
             return self.ProductStorage.postProduct(name, value)
-
