1e040f7c6a54c546846ee1b5b825fcf7e951d301
Maciej <maciej.tokarz1@wp.pl>
2021-01-09 11:55:42 +0100

    test bad new value, method editProduct in class Product RED

--- tests/test_edit_product.py
+++ tests/test_edit_product.py
@@ -15,3 +15,9 @@ class testEditProduct(TestCase):
     def test_edit_product_bad_new_name(self):
         result = self.temp.editProduct
         self.assertRaisesRegex(TypeError, "Bad type new name", result, 7, True, str)
+
+    def test_edit_product_bad_new_value(self):
+        self.temp.editProduct = MagicMock()
+        self.temp.editProduct.side_effect = TypeError("Bad type new value")
+        result = self.temp.editProduct
+        self.assertRaisesRegex(TypeError, "Bad type new value", result, 7, "kite", "22")
