5ceafa6b4e28ab99f6acf5427a6fb1f850c13546
Maciej <maciej.tokarz1@wp.pl>
2021-01-09 15:22:26 +0100

    test product not exists, method addOrder in class Order RED

--- tests/test_add_order.py
+++ tests/test_add_order.py
@@ -2,12 +2,14 @@ from unittest.mock import *
 from unittest import TestCase, main
 from src.serviceOrders import Order
 from src.dataClients import ClientsData
+from src.dataProducts import ProductsData
 
 
 class testAddOrder(TestCase):
     def setUp(self):
         self.temp = Order()
         self.clients = ClientsData().clients
+        self.products = ProductsData().products
 
     def test_add_order_bad_id_client(self):
         result = self.temp.addOrder
@@ -26,6 +28,16 @@ class testAddOrder(TestCase):
 
         result = self.temp.addOrder
         self.assertRaisesRegex(TypeError, "Bad type cart", result, 5, "ball")
+
+    def test_add_order_product_not_exits(self):
+        self.temp.ClientStorage.getAllClients = Mock()
+        self.temp.ClientStorage.getAllClients.return_value = self.clients
+
+        self.temp.ProductStorage.getAllProducts = MagicMock()
+        self.temp.ProductStorage.getAllProducts.return_value = self.products
+
+        result = self.temp.addOrder
+        self.assertRaisesRegex(Exception, "This product not exist", result, 3, ["ball", "trampoline"])
 
     def tearDown(self):
         self.temp = None
