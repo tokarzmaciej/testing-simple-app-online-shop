23c81d12d771057e580fccdd2cfc4297952f785b
Maciej <maciej.tokarz1@wp.pl>
2021-01-09 10:27:12 +0100

    test bad value, method addProduct in class Product GREEN

--- src/serviceProducts.py
+++ src/serviceProducts.py
@@ -3,3 +3,8 @@ class Product:
     def addProduct(self, name, value):
         if type(name) != str:
             raise TypeError("Bad type name")
+        elif type(value) != int:
+            raise TypeError("Bad type value")
+        elif type(value) != float:
+            raise TypeError("Bad type value")
+
