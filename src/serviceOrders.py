c8e60cf2bd8029265a6996721712b8cac2d8e34c
Maciej <maciej.tokarz1@wp.pl>
2021-01-09 22:05:21 +0100

    test get products in order positive, method getProductsInOrder in class Order REFACTOR

--- src/serviceOrders.py
+++ src/serviceOrders.py
@@ -46,6 +46,8 @@ class Order:
             return self.OrderStorage.delOrder(id_order)
 
     def deleteOrderProduct(self, id_order, id_product):
+        ordersProducts = self.OrderStorage.getAllOrdersProducts()
+
         if type(id_order) != int:
             raise TypeError("Bad type order id")
 
@@ -53,21 +55,26 @@ class Order:
             raise TypeError("Bad type product id")
 
         if not list(filter(lambda order: order["order_id"] == int(id_order) and order["product_id"] == int(id_product),
-                           self.OrderStorage.getAllOrdersProducts())):
+                           ordersProducts)):
             raise Exception("This order not exist in data base")
         else:
             return self.OrderStorage.delOrderProduct(id_order, id_product)
 
     def getProductsInOrder(self, id_order):
+        orders = self.OrderStorage.getAllOrders()
+        ordersProducts = self.OrderStorage.getAllOrdersProducts()
+        products = self.ProductStorage.getAllProducts()
+
         if type(id_order) != int:
             raise TypeError("Bad type order id")
-        if len(list(filter(lambda order: order["id"] == int(id_order), self.OrderStorage.getAllOrders()))) != 1:
-            raise Exception("This order not exist in data base")
-        else:
+
+        if len(list(filter(lambda order: order["id"] == int(id_order), orders))) == 1:
             look_products_id = list(
-                filter(lambda order_product: order_product["order_id"] == int(id_order), self.OrderStorage.getAllOrdersProducts()))
+                filter(lambda order_product: order_product["order_id"] == int(id_order), ordersProducts))
             list_found_products = []
             for product_id in look_products_id:
-                found_product = list(filter(lambda product: product["id"] == product_id["product_id"], self.ProductStorage.getAllProducts()))
+                found_product = list(filter(lambda product: product["id"] == product_id["product_id"], products))
                 list_found_products.append(found_product[0])
             return list_found_products
+        else:
+            raise Exception("This order not exist in data base")
