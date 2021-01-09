c1af63713af9d78c9ca4f3bf2cfea5d86233ab1f
Maciej <maciej.tokarz1@wp.pl>
2021-01-09 23:07:11 +0100

    test get product by name positive, method getProductByName in class Product RED

--- tests/test_get_product.py
+++ tests/test_get_product.py
@@ -1,5 +1,6 @@
 from unittest.mock import *
 from unittest import TestCase, main
+from assertpy import assert_that
 from src.serviceProducts import Product
 from src.dataProducts import ProductsData
 
@@ -21,6 +22,19 @@ class testGetByNameProduct(TestCase):
 
         result = Product().getProductByName
         self.assertRaisesRegex(Exception, "This product exist", result, "shoe")
+
+    @patch('src.serviceProducts.ProductStorage')
+    def test_get_product_by_name_positive(self, products):
+        instance_products = Mock()
+        instance_products.getAllProducts.return_value = self.products
+        products.return_value = instance_products
+
+        result = Product().getProductByName("ball")
+        assert_that(result).contains_only({
+            "id": 2,
+            "name": "ball",
+            "value": 50
+        })
 
     def tearDown(self):
         self.temp = None
