cc26c21af45e57985ffc3ca51e0ce1f6673334ac
Maciej <maciej.tokarz1@wp.pl>
2021-01-09 16:01:26 +0100

    test add order with few products verification mock, method addOrder in class Order REFACTOR

--- src/serviceOrders.py
+++ src/serviceOrders.py
@@ -10,11 +10,12 @@ class Order:
         self.SpyPostOrder = SpyPostOrder()
 
     def addOrder(self, id_client, cart):
+        clients = self.ClientStorage.getAllClients()
         products = self.ProductStorage.getAllProducts()
 
         if type(id_client) != int:
             raise TypeError("Bad type id")
-        if len(list(filter(lambda client: client["id"] == id_client, self.ClientStorage.getAllClients()))) != 1:
+        if len(list(filter(lambda client: client["id"] == id_client, clients))) != 1:
             raise Exception("This client not exist in data base")
         if type(cart) != list:
             raise TypeError("Bad type cart")
