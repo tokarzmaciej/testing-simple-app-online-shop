6a92bd45ebbf08f2bb779ff109f1053b2ff2cdde
Maciej <maciej.tokarz1@wp.pl>
2021-01-09 19:35:07 +0100

    test order not exists, method deleteOrder in class Order RED

--- tests/test_delete_order.py
+++ tests/test_delete_order.py
@@ -1,11 +1,13 @@
 from unittest.mock import *
 from unittest import TestCase, main
 from src.serviceOrders import Order
+from src.dataOrders import OrdersData
 
 
 class testDeleteOrder(TestCase):
     def setUp(self):
         self.temp = Order()
+        self.orders = OrdersData().orders
 
     def test_delete_order_bad_id_order(self):
         self.temp.deleteOrder = MagicMock()
@@ -13,6 +15,13 @@ class testDeleteOrder(TestCase):
 
         result = self.temp.deleteOrder
         self.assertRaisesRegex(TypeError, "Bad type id order", result, "1")
+
+    def test_delete_order_not_exist_order(self):
+        self.temp.OrderStorage.getAllOrders = MagicMock()
+        self.temp.OrderStorage.getAllOrders.return_value = self.orders
+
+        result = self.temp.deleteOrder
+        self.assertRaisesRegex(Exception, "This order not exist in data base", result, 101)
 
     def tearDown(self):
         self.temp = None
