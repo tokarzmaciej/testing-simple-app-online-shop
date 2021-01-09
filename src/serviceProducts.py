2d150c76996bf041488906cf75926fffc73d6e47
Maciej <maciej.tokarz1@wp.pl>
2021-01-09 11:08:25 +0100

    test bad id, method deleteProduct in class Product REFACTOR

--- src/serviceProducts.py
+++ src/serviceProducts.py
@@ -17,5 +17,5 @@ class Product:
             return self.ProductStorage.postProduct(name, value)
 
     def deleteProduct(self, id_product):
-        raise TypeError("Bad type id product")
-
+        if type(id_product) != int:
+            raise TypeError("Bad type id product")
