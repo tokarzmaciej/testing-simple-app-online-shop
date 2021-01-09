9a05f10fd26dabe571c65b7ee935a7c508d19466
Maciej <maciej.tokarz1@wp.pl>
2021-01-09 10:34:49 +0100

    test product already exists, method addProduct in class Product RED

--- tests/test_add_product.py
+++ tests/test_add_product.py
@@ -15,6 +15,13 @@ class testAddProduct(TestCase):
         result = self.temp.addProduct
         self.assertRaisesRegex(TypeError, "Bad type value", result, "kite", "22")
 
+    @patch.object(Product, 'addProduct')
+    def test_add_product_already_exist(self, mock_method):
+        mock_method.side_effect = Exception("This prodct exist")
+
+        result = self.temp.addProduct
+        self.assertRaisesRegex(Exception, "This product exist", result, "ball", 50)
+
     def tearDown(self):
         self.temp = None
 
