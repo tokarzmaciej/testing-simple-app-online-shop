f212b137430c1e759f144bcca3e40404b9fe884d
Maciej <maciej.tokarz1@wp.pl>
2021-01-09 11:58:41 +0100

    test bad new value, method editProduct in class Product REFACTOR

--- src/serviceProducts.py
+++ src/serviceProducts.py
@@ -7,10 +7,13 @@ class Product:
 
     def addProduct(self, name, value):
         products = self.ProductStorage.getAllProducts()
+
         if type(name) != str:
             raise TypeError("Bad type name")
+
         if type(value) != int and type(value) != float:
             raise TypeError("Bad type value")
+
         if list(filter(lambda product: product["name"] == name, products)):
             raise Exception("This product exist")
         else:
@@ -18,8 +21,10 @@ class Product:
 
     def deleteProduct(self, id_product):
         products = self.ProductStorage.getAllProducts()
+
         if type(id_product) != int:
             raise TypeError("Bad type id product")
+
         if len(list(filter(lambda product: product["id"] == int(id_product), products))) == 1:
             return self.ProductStorage.delProduct(id_product)
         else:
@@ -30,6 +35,5 @@ class Product:
             raise TypeError("Bad type id product")
         if type(new_name) != str and new_name is not None:
             raise TypeError("Bad type new name")
-        else:
+        if type(new_value) != int and type(new_value) != float and new_value is not None:
             raise TypeError("Bad type new value")
-
