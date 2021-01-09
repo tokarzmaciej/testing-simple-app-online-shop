47d670d61cf3f7fa664a0dacedc61f45da3f533d
Maciej <maciej.tokarz1@wp.pl>
2021-01-09 17:11:20 +0100

    test add order positive, method addOrder in class Order REFACTOR

--- src/serviceOrders.py
+++ src/serviceOrders.py
@@ -11,20 +11,21 @@ class Order:
         self.SpyPostOrder = SpyPostOrder()
         self.OrderStorage = OrderStorage()
 
-
     def addOrder(self, id_client, cart):
         clients = self.ClientStorage.getAllClients()
         products = self.ProductStorage.getAllProducts()
 
         if type(id_client) != int:
             raise TypeError("Bad type id")
+
         if len(list(filter(lambda client: client["id"] == id_client, clients))) != 1:
             raise Exception("This client not exist in data base")
+
         if type(cart) != list:
             raise TypeError("Bad type cart")
+
         for element in cart:
-            product_in_base = list(
-                filter(lambda product: product["name"] == element, products))
+            product_in_base = list(filter(lambda product: product["name"] == element, products))
             if not product_in_base:
                 raise Exception("This product not exist")
             else:
